9adee6af3cbe0ee07e00e52e82ec090b0187111f
Edgar <A01113146@itesm.mx>
2019-07-10 11:39:18 -0500

    Finish static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,30 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+
+  test "should get route" do
+    get root_url
+    assert_response :success
+  end
+  test "should get home" do
+    get home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
 end
